7a67f9c68ec32f7293e9cec149f2537378628ca1
HyeonminNam <goisugn@gmail.com>
2020-09-14 02:47:40 +0900

    전처리 함수 매개변수 이름 수정, del_emoji 함수 수정

--- TC_preprocessing.py
+++ TC_preprocessing.py
@@ -1,33 +1,39 @@
-import json
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
 import re
+import emoji
 # from pykospacing import spacing # 설치 방법 : pip install git+https://github.com/haven-jeon/PyKoSpacing.git
 
 class preprocessing () :
     
     def __init__(self):
         self.escape_code = ['\n', '\xa0', '\"', '\'', '\t', '\r', '\$', '\\', '\u200d']
+        self.emoji_dic = emoji.UNICODE_EMOJI
+        self.emoji_list = map(lambda x: ''.join(x.split()), emoji.UNICODE_EMOJI.keys())
+        self.re_emoji = re.compile('|'.join(re.escape(p) for p in self.emoji_list if p != '\u200d|\u200c'))
         return
     
     # hashtag 추출(#포함)
-    def extract_hashtag(self, content) :
-        hashtag_list = re.findall('\#[\w가-힣a-zA-Z0-9]*', str(content))
+    def extract_hashtag(self, text) :
+        hashtag_list = re.findall('\#[\w가-힣a-zA-Z0-9]*', str(text))
         if hashtag_list:
             return hashtag_list
         else :
             return ''
     
     # post 추출
-    def extract_post(self, content) :  
-        post = re.sub('\#[\w가-힣a-zA-Z0-9]*',"",str(content)) 
+    def extract_post(self, text) :  
+        post = re.sub('\#[\w가-힣a-zA-Z0-9]*',"",str(text)) 
         post = self.del_escape(post)
         post = re.sub("\@[\w가-힣a-zA-Z0-9]*","",post)   
         return post #string type
 
     # 태그된 userID 추출
-    def extract_tagged_userID(self, content) : #태그된 userID 추출의 경우 hashtag 추출과 달리 @를 제거해준 값 리턴 
-        re_content = re.findall('\@[\w가-힣a-zA-Z0-9]*', str(content))
+    def extract_tagged_userID(self, text) : #태그된 userID 추출의 경우 hashtag 추출과 달리 @를 제거해준 값 리턴 
+        re_text = re.findall('\@[\w가-힣a-zA-Z0-9]*', str(text))
         userID_list= []
-        for userID in re_content:
+        for userID in re_text:
             userID_list.append(re.sub("@","",userID))
         return userID_list
 
@@ -40,43 +46,40 @@ class preprocessing () :
         return tmp
     
     # # pykospacing패키지를 사용한 띄어쓰기 처리
-    # def auto_spacing(self, content) :
-    #     return spacing(content)
+    # def auto_spacing(self, text) :
+    #     return spacing(text)
         
     # Escape Code 처리
-    def del_escape(self, content):
+    def del_escape(self, text):
         for e in self.escape_code:
-            content = content.replace(e, ' ')
-        return content
+            text = text.replace(e, ' ')
+        return text
     
     # emoji 삭제
-    def del_emoji(self, content) :
-        only_BMP_pattern = re.compile("["
-        u"\U00010000-\U0010FFFF"  #BMP characters 이외
-                           "]+", flags=re.UNICODE)
-        return only_BMP_pattern.sub(r'', content)
+    def del_emoji(self, text) :
+        return self.re_emoji.sub(r'', text)
     
-    # content list 전처리
-    def preprocess_content(self, content_list, spacing = False, sub_hash = False) :        
+    # text list 전처리
+    def preprocess_text(self, text_list, sub_hash = False) :        
         post_list =[]
         hashtag_list= []         
-        for content in content_list :
-            original_post = self.extract_post(content)
-            if spacing :
-                # post_list.append(self.auto_spacing(original_post))
-                pass
-            else :
-                post_list.append(original_post)
+        for text in text_list :
+            original_post = self.extract_post(text)
+            # if spacing :
+            #     # post_list.append(self.auto_spacing(original_post))
+            #     pass
+            # else :
+            post_list.append(original_post)
             if sub_hash :
-                hashtag_list.append(self.remove_hash(self.extract_hashtag(content)))
+                hashtag_list.append(self.remove_hash(self.extract_hashtag(text)))
             else :
-                hashtag_list.append(self.extract_hashtag(content))
+                hashtag_list.append(self.extract_hashtag(text))
         return post_list, hashtag_list
     
 if __name__ == "__main__":
-    content_list = ['다이어트 해야되는데...😂😂\n.\n.\n.\n#멋짐휘트니스연산점 #연산동pt','럽스타 그자체❤❤ #럽스타그램 #운동하는커플 #연산동pt']
+    text_list = ['다이어트 해야되는데...😂😂\n.\n.\n.\n#멋짐휘트니스연산점 #연산동pt','럽스타 그자체❤❤ #럽스타그램 #운동하는커플 #연산동pt']
     test_class = preprocessing()
-    post_ls, hashtag_ls = test_class.preprocess_content(content_list, spacing= True)
+    post_ls, hashtag_ls = test_class.preprocess_text(text_list)
     print(post_ls)
     print(hashtag_ls)
     print("-----------------------------------------------------------------------------------------------")
